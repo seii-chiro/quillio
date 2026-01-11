785b552533f97a508752efe32239abb4698fa1fe
Rhay from Mac <javier.johnrhay@gmail.com>
2026-01-11 14:09:53 +0800

    feat: update StyledInput component to accept props and render as an input element

--- src/components/styled-input.tsx
+++ src/components/styled-input.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
-
-const StyledInput = () => {
+const StyledInput = ({ ...props }) => {
   return (
-    <div>StyledInput</div>
+    <input
+      className="h-9 border border-gray-200 rounded-md px-2 focus:outline-none focus:ring-0 focus:border-black"
+      {...props}
+    />
   )
 }
 
