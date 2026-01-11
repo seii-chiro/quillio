8b9f84a3467d1ba5a008847a225f1ae459a0abbf
Rhay from Mac <javier.johnrhay@gmail.com>
2026-01-11 16:55:12 +0800

    feat: implement LogoutBtn component and update Navigation for logout functionality

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,17 +1,42 @@
-import { Link } from "react-router"
+import { NavLink } from "react-router"
+import LogoutBtn from "./LogoutBtn"
 
 const Navigation = () => {
     return (
-        <nav>
-            <ul className="flex gap-4">
+        <nav className="bg-white rounded-full px-6 py-2 shadow-xl">
+            <ul className="flex items-center gap-8">
                 <li>
-                    <Link to={"/"}>Home</Link>
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive ? 'text-sky-600 pb-1 border-b-2 border-sky-600' : 'text-slate-700 hover:text-slate-900'
+                        }
+                    >
+                        Home
+                    </NavLink>
                 </li>
                 <li>
-                    <Link to={"/my-blogs"}>My Blogs</Link>
+                    <NavLink
+                        to="/my-blogs"
+                        className={({ isActive }) =>
+                            isActive ? 'text-sky-600 pb-1 border-b-2 border-sky-600' : 'text-slate-700 hover:text-slate-900'
+                        }
+                    >
+                        My Blogs
+                    </NavLink>
                 </li>
                 <li>
-                    <Link to={"/settings"}>Settings</Link>
+                    <NavLink
+                        to="/settings"
+                        className={({ isActive }) =>
+                            isActive ? 'text-sky-600 pb-1 border-b-2 border-sky-600' : 'text-slate-700 hover:text-slate-900'
+                        }
+                    >
+                        Settings
+                    </NavLink>
+                </li>
+                <li>
+                    <LogoutBtn />
                 </li>
             </ul>
         </nav>
