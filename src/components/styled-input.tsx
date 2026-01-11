7f1afb8afcd9ece077bfe2c706c8df3ce52e45f5
Rhay from Mac <javier.johnrhay@gmail.com>
2026-01-11 14:50:07 +0800

    feat: add full width styling to StyledInput component

--- src/components/styled-input.tsx
+++ src/components/styled-input.tsx
@@ -1,7 +1,7 @@
 const StyledInput = ({ ...props }) => {
   return (
     <input
-      className="h-9 border border-gray-200 rounded-md px-2 focus:outline-none focus:ring-0 focus:border-black"
+      className="w-full h-9 border border-gray-200 rounded-md px-2 focus:outline-none focus:ring-0 focus:border-black"
       {...props}
     />
   )
