7a8d143ca1922f8c21ab40399ac12a59fb663741
Rhay from Mac <javier.johnrhay@gmail.com>
2026-01-11 18:08:10 +0800

    feat: enhance MyBlogs page layout and integrate CreateBlog component

--- src/pages/MyBlogs.tsx
+++ src/pages/MyBlogs.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import CreateBlog from '../components/CreateBlog'
 
 const MyBlogs = () => {
     return (
-        <div>MyBlogs</div>
+        <div className="px-4">
+            <h2 className="text-2xl font-semibold mb-6">My Blogs</h2>
+
+            <div className='w-full flex justify-center'>
+                <div className="w-full max-w-2xl">
+                    <CreateBlog />
+                </div>
+            </div>
+        </div>
     )
 }
 
