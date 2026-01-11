a8b36fecab4fcc353ce6ac6bff7c246178cafcff
Rhay from Mac <javier.johnrhay@gmail.com>
2026-01-11 15:14:35 +0800

    feat: add required attribute to StyledInput component

--- src/components/styled-input.tsx
+++ src/components/styled-input.tsx
@@ -1,6 +1,7 @@
 const StyledInput = ({ ...props }) => {
   return (
     <input
+      required
       className="w-full h-9 border border-gray-200 rounded-md px-2 focus:outline-none focus:ring-0 focus:border-black"
       {...props}
     />
