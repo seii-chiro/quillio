9920d665b8dd4abc30ccd5b62323b6b19c99fe26
Rhay from Workstation <javier.johnrhay@gmail.com>
2026-01-13 13:35:56 +0800

    feat: enhance BlogCard component with edit and delete functionality for user blogs

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -2,20 +2,30 @@ import { Link } from "react-router";
 import { FaUser, FaCalendarAlt, FaLock } from "react-icons/fa";
 import type { BlogWithAuthor } from "../types/blogs";
 import { formatDate } from "../utils/formatDate";
+import EditBlogModalBtn from "./EditBlogModalBtn";
+import ConfirmDeleteBlogBtn from "./ConfirmDeleteBlogBtn";
 
 type BlogCardProps = {
     blog: BlogWithAuthor;
+    isInMyBlogs?: boolean;
 };
 
 
-const BlogCard = ({ blog }: BlogCardProps) => {
+const BlogCard = ({ blog, isInMyBlogs }: BlogCardProps) => {
     const { title, author, created_at, body, only_me, id } = blog;
     const authorName = author?.display_name ?? "Unknown";
 
     return (
         <article className="bg-white rounded-lg shadow-sm p-5 hover:shadow-md transition">
             <header>
-                <h3 className="text-lg font-semibold text-slate-800">{title}</h3>
+                <div className="w-full flex items-center justify-between">
+                    <h3 className="text-lg font-semibold text-slate-800">{title}</h3>
+                    {
+                        isInMyBlogs && (
+                            <ConfirmDeleteBlogBtn blogId={blog.id} />
+                        )
+                    }
+                </div>
                 <div className="mt-2 flex items-center gap-3 text-sm text-slate-500">
                     <span className="flex items-center gap-2">
                         <FaUser className="text-slate-400" />
@@ -36,8 +46,12 @@ const BlogCard = ({ blog }: BlogCardProps) => {
 
             <p className="mt-4 text-sm text-slate-700">{body}</p>
 
-            <div className="mt-4 flex items-center justify-between">
-                <div className="text-xs text-slate-400"></div>
+            <div className="mt-4 flex items-center justify-end gap-3">
+                {
+                    isInMyBlogs && (
+                        <EditBlogModalBtn blog={blog} />
+                    )
+                }
                 <Link to={`/blogs/${id}`} className="text-sm bg-sky-600 text-white px-3 py-1.5 rounded hover:bg-sky-700 transition">
                     Read
                 </Link>
