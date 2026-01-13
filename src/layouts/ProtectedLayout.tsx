6751a8b60ccaf55e52a6f4e41c742a29e72201e8
Rhay from Workstation <javier.johnrhay@gmail.com>
2026-01-13 13:36:35 +0800

    feat: enhance ProtectedLayout with session check and user dispatch

--- src/layouts/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayout.tsx
@@ -3,9 +3,12 @@ import { Outlet, useNavigate } from "react-router"
 import { supabase } from "../supabaseClient";
 import Header from "../components/Header";
 import Navigation from "../components/Navigation";
+import { setUser } from "../slice/userSlice";
+import { useAppDispatch } from "../hooks/store-hooks";
 
 const ProtectedLayout = () => {
     const navigate = useNavigate();
+    const dispatch = useAppDispatch();
 
     useEffect(() => {
         const checkSession = async () => {
@@ -15,16 +18,18 @@ const ProtectedLayout = () => {
                 navigate("/login");
                 return;
             }
+
+            dispatch(setUser(data?.session?.user));
         }
         checkSession();
-    }, [navigate])
+    }, [navigate, dispatch])
 
     return (
-        <div className="w-full h-full relative">
+        <div className="w-full h-full flex flex-col items-center relative">
             <Header>
                 <Navigation />
             </Header>
-            <main>
+            <main className="w-[60%] flex flex-col items-center">
                 <Outlet />
             </main>
         </div>
